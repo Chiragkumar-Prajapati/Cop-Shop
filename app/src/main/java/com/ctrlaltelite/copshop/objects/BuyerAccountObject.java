80a29caaef806f94763b52f0a8dbad53e2ad00b2
Katie <ummartch@myumanitoba.ca>
2018-06-12 22:06:37 -0500

    -BuyerAccountObject updated

--- app/src/main/java/com/ctrlaltelite/copshop/objects/BuyerAccountObject.java
+++ app/src/main/java/com/ctrlaltelite/copshop/objects/BuyerAccountObject.java
@@ -12,11 +12,75 @@ public class BuyerAccountObject extends AccountObject {
     private String password;
 
     public BuyerAccountObject(String id, String firstName, String lastName, String streetAddress,
-                              String postalCode, String provice, String email, String password) {
+                              String postalCode, String province, String email, String password) {
         super(id, firstName, password, email);
 
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.streetAddress = streetAddress;
+        this.postalCode = postalCode;
+        this.province = province;
+        this.email = email;
+        this.password = password;
     }
 
+    //Getters
 
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getStreetAddress() {return streetAddress; }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public String getEmail() { return email;}
+
+    public String getPassword() {
+        return password;
+    }
+
+    //Setters
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setStreetAddress(String streetAddress) {this.streetAddress = streetAddress; }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public void setProvince(String province) {
+        this.province= province;
+    }
+
+    public void setEmail(String email) { this.email = email;}
+
+    public void setPassword(String password) { this.password = password;}
 
 }
