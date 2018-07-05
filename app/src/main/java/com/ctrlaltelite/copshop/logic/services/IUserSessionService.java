e2cccc580b15299a734da854b37fbd12fa012406
Katie <ummartch@myumanitoba.ca>
2018-07-05 00:12:28 -0500

    !48 smelliness clean up added some comments for UserSessionService interface

--- app/src/main/java/com/ctrlaltelite/copshop/logic/services/IUserSessionService.java
+++ app/src/main/java/com/ctrlaltelite/copshop/logic/services/IUserSessionService.java
@@ -2,14 +2,30 @@ package com.ctrlaltelite.copshop.logic.services;
 
 public interface IUserSessionService {
 
+    /* Check if there is currently a user logged in*/
     boolean userLoggedIn();
 
+    //getters
     String getUserEmail();
     String getUserID();
     String getUserType();
 
+    /**
+     *
+     * @param email The email of the current user logged in to app
+     */
     void setUserEmail(String email);
+
+    /**
+     *
+     * @param id The user ID of the current user logged in to app
+     */
     void setUserID(String id);
+
+    /**
+     *
+     * @param type The type of user currently logged in to app
+     */
     void setUserType(String type);
 
 }
